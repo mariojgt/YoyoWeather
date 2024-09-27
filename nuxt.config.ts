845d301dce0e9243e6ba2a34060b0428ba84e2ba
Mario Tarosso <mariojgt2@gmail.com>
2024-09-27 13:04:41 +0100

    Deploy Action

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
-    modules: ["@nuxt/ui"]
+    modules: ["@nuxt/ui"],
+    target: 'static', // Ensure static site generation
+    router: {
+        base: '/YoyoWeather/', // Your repository name
+    }
 });
